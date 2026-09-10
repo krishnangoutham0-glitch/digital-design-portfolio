b097e5053740dd867c1beff0c2fa557573af397a
Goutham Krishnan <krishnangoutham0@gmail.com>
2026-09-11 05:10:54 +0530

    Update register_file.v

--- Tiny_Instruction_Executor/rtl/register_file.v
+++ Tiny_Instruction_Executor/rtl/register_file.v
@@ -1,3 +1,5 @@
+`timescale 1ns/1ps
+
 module register_file #(
     parameter DATA_WIDTH = 8,
     parameter REG_COUNT  = 16
@@ -16,41 +18,30 @@ module register_file #(
     output [DATA_WIDTH-1:0] read_data2
 );
 
-//----------------------------------------------------
-// Internal Register Array
-//----------------------------------------------------
+    reg [DATA_WIDTH-1:0] reg_file [0:REG_COUNT-1];
 
-reg [DATA_WIDTH-1:0] reg_file [0:REG_COUNT-1];
+    integer i;
 
-// Loop variable used during reset
-integer i;
-
-//----------------------------------------------------
-// Write Logic
-//----------------------------------------------------
-
-always @(posedge clk or posedge reset)
-begin
-    if (reset)
+    always @(posedge clk or posedge reset)
     begin
-        // Clear every register
-        for (i = 0; i < REG_COUNT; i = i + 1)
-            reg_file[i] <= 0;
+        if (reset)
+        begin
+            for (i = 0; i < REG_COUNT; i = i + 1)
+            begin
+                reg_file[i] <= {DATA_WIDTH{1'b0}};
+            end
+        end
+        else
+        begin
+            if (write_enable)
+            begin
+                reg_file[write_addr] <= write_data;
+            end
+        end
     end
 
-    else if (write_enable)
-    begin
-       
-        reg_file[write_addr] <= write_data;
-    end
-end
+    assign read_data1 = reg_file[read_addr1];
 
-//----------------------------------------------------
-// Read Logic
-//----------------------------------------------------
-
-assign read_data1 = reg_file[read_addr1];
-
-assign read_data2 = reg_file[read_addr2];
+    assign read_data2 = reg_file[read_addr2];
 
 endmodule
